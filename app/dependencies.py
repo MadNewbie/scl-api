46d7efd2f79abee5efda28aa06f51759c7e345c9
Rachmad Yanuarianto <rachmad.y25@gmail.com>
2026-06-22 15:15:59 +0700

    handling when token

--- app/dependencies.py
+++ app/dependencies.py
@@ -9,6 +9,19 @@ from .database import get_db
 def check_token_header(authorization: Annotated[str, Header()], db: Session = Depends(get_db)):
     token = authorization[7:]
     stmtUser = text("SELECT * FROM public.fc_api_csbs_get_user_secret_by_token(:token)")
+    user = db.execute(stmtUser,{"token": token}).mappings().all()
+    if user is None or user == []:
+        print("Token Not Found", e)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail = {
+                "code": 401,
+                "status": "Unauthorized",
+                "external_code": -100011,
+                "external_desc": "Authentication Fail",
+                "errors": ["Invalid Token"]
+            }
+        )
     secret = db.execute(stmtUser,{"token": token}).mappings().all()[0].secret
     dbUser = db.execute(stmtUser,{"token": token}).mappings().all()[0].user_id
     # print(secret, dbUser)
